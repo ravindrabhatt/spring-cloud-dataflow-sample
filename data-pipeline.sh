336b534d97e6404a6de9a122a1f20bd776e85384
Ravindra Kumar Bhatt <ravindra.bhatt29@gmail.com>
2017-07-01 10:28:23 +0530

    Fix data-pipeline.sh

--- data-pipeline.sh
+++ data-pipeline.sh
@@ -2,8 +2,10 @@ app register --name http-source --type source --uri maven://org.kun:spring-cloud
 
 app register --name transform-processor --type processor --uri maven://org.kun:spring-cloud-data-flow-processor:jar:0.0.1-SNAPSHOT
 
-app register --name logging-sink --type sink --uri maven://org.kun:spring-cloud-data-flow-logging-sink:jar:0.0.1-SNAPSHOT
+app register --name logging-sink --type sink --uri maven://org.kun:spring-cloud-data-flow-sink:jar:0.0.1-SNAPSHOT
 
 stream create --name transform --definition 'http-source --server.port=8000 | transform-processor --expression=payload.toUpperCase() | logging-sink'
 
 stream deploy --name transform
+
+# curl -X POST http://localhost:8000/kun  -H 'content-type: text/plain'  -d 'make this uppercase'
